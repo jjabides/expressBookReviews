8b269bfa59028230bc3f9ec91cbbbf31b968acd0
jjabides <jj.abides@gmail.com>
2026-01-04 14:15:02 -0500

    Add code for login endpoint

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -5,24 +5,45 @@ const regd_users = express.Router();
 
 let users = [];
 
-const isValid = (username)=>{ //returns boolean
-//write code to check is the username is valid
+const isValid = (username) => { //returns boolean
+    //write code to check is the username is valid
 }
 
-const authenticatedUser = (username,password)=>{ //returns boolean
-//write code to check if username and password match the one we have in records.
+const authenticatedUser = (username, password) => { //returns boolean
+    //write code to check if username and password match the one we have in records.
+    let user = users.find(u => u.username === username && u.password === password)
+    return user !== undefined
 }
 
 //only registered users can login
-regd_users.post("/login", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+regd_users.post("/login", (req, res) => {
+    //Write your code here
+    const username = req.body.username
+    const password = req.body.password
+
+    if (!username || !password) {
+        return res.status(404).json({ message: "Credentials not provided" })
+    }
+
+    if (authenticatedUser(username, password)) {
+        const accessToken = jwt.sign({
+            data: password
+        }, "access", { expiresIn: 60 * 60 })
+
+        req.session.authentication = {
+            accessToken,
+            username
+        }
+        return res.status(200).json({ message: "User successfully logged in"})
+    } else {
+        return res.status(208).json({ message: "Invalid login" })
+    }
 });
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    //Write your code here
+    return res.status(300).json({ message: "Yet to be implemented" });
 });
 
 module.exports.authenticated = regd_users;
